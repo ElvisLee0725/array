c87bfecec3cc7784872967aa85d38650198bded7
ElvisLee <elvislee0725@gmail.com>
2021-01-26 12:11:14 -0800

    modify

--- CreateTargetArray/src/Solution.java
+++ CreateTargetArray/src/Solution.java
@@ -1,5 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
+// Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+//
+//Initially target array is empty.
+//From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+//Repeat the previous step until there are no elements to read in nums and index.
+//Return the target array.
+//
+//It is guaranteed that the insertion operations will be valid.
+
+// 1 <= nums.length, index.length <= 100
+// nums.length == index.length
+// 0 <= nums[i] <= 100
+// 0 <= index[i] <= i
 
 // Use a arraylist with capacity initialized. (Not size!!) to reduce memory re-allocation time.
 // Since the insertion is always valid, it always starts from index 0, and only insert at places
