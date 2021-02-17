0125e46808cc3ae6b897cfe2aa0d1216ba6fdf22
ElvisLee <elvislee0725@gmail.com>
2021-02-17 11:10:53 -0800

    Add Recursive solution

--- SpiralMatrix/src/Solution.java
+++ SpiralMatrix/src/Solution.java
@@ -63,4 +63,58 @@ class Solution {
         }
         return res;
     }
+
+    // Recursive Solution:
+    // Like an onion, start from the outer layer, iterate top, right, bottom, and left edges, then go to inner layer. At the inner layer, the row and col decrease by 2
+    // Base Case: when row and col are both 1, it's the last element. When either of them reaches 0, it's done
+
+        public List<Integer> spiralOrderRecursion(int[][] matrix) {
+            List<Integer> res = new ArrayList();
+            traverse(matrix, matrix.length, matrix[0].length, res, 0);
+            return res;
+        }
+
+        public void traverse(int [][] m, int r, int c, List<Integer> res, int layer) {
+            if(r == 0 || c == 0) {
+                return ;
+            }
+            else if(r == 1 && c == 1) {
+                res.add(m[layer][layer]);
+                return ;
+            }
+            else if(r == 1) {
+                for(int i = 0; i < c; i++) {
+                    res.add(m[layer][layer+i]);
+                }
+                return ;
+            }
+            else if(c == 1) {
+                for(int i = 0; i < r; i++) {
+                    res.add(m[layer+i][layer]);
+                }
+                return ;
+            }
+
+            // Top Edge: left to right
+            for(int i = 0; i < c - 1; i++) {
+                res.add(m[layer][layer+i]);
+            }
+
+            // Right Edge: top to bottom
+            for(int i = 0; i < r - 1; i++) {
+                res.add(m[layer+i][c-1+layer]);
+            }
+
+            // Bottom Edge: right to left
+            for(int i = c - 1; i > 0; i--) {
+                res.add(m[r-1+layer][layer+i]);
+            }
+
+            // Left Edge: bottom to top
+            for(int i = r - 1; i > 0; i--) {
+                res.add(m[layer+i][layer]);
+            }
+
+            traverse(m, r - 2, c - 2, res, layer + 1);
+        }
 }
